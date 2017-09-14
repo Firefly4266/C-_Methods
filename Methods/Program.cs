999536e73c223bd7d74cb4cf04a4158e0672d26b
Firefly4266 <gaines.johnp@gmail.com>
2017-09-14 10:05:11 -0700

    Problem 22 completed

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,6 +10,25 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            Greet();
+            Math();
+            Ceeya();
+        }
+        static void Greet()
+        {
+            Console.Write("\n Greetings User! How are you doing today? ");
+            string userInput = Console.ReadLine();
+        }
+        static void Math()
+        {
+            Console.Write($"\n Help me with some basic math.  What is 2 + 2 / 2? ");
+            Console.ReadLine();
+        }
+        static void Ceeya()
+        {
+            Console.Write("\n Oh, I'm sorry.  Were you waiting for a response? ");
+            Console.ReadLine();
+            Console.WriteLine("\n Well I hope you're not holding your breath!\n");
         }
     }
 }
