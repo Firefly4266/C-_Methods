10f9e99d39243b7630e87e4791fa2ef6fca2eae6
Firefly4266 <gaines.johnp@gmail.com>
2017-09-13 19:20:49 -0700

    Incomplete, while loop done but still needs to be finished.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,6 +10,32 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            CountToTen();
+            GetNumberFromUser();
+            Count(5);
+            Count(15);
+        }
+        static void CountToTen()
+        {
+            for (var i = 1; i <= 10; i++)
+            Console.WriteLine(i);
+        }
+        static int GetNumberFromUser()
+        {
+            int userNumber = 0;
+
+            while (userNumber < 1 || userNumber > 10)
+            {
+                Console.Write("Greetings user, please enter a number between 1 and 10: ");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(userNumber);
+            return userNumber;
+        }
+        static int Count(int numberToCountTo)
+        {
+            for (var current = 1; current < numberToCountTo; current++)
+                Console.WriteLine(current);
         }
     }
 }
